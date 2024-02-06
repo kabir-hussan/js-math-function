c82d3910b14fbd3742e265ca4b85297610652e47
kabir-hussan <kabirhussan891@gmail.com>
2024-02-07 00:56:18 +0600

    math related function

--- math.js
+++ math.js
@@ -7,9 +7,15 @@ const max= Math.max(44, 66, 77, 22, 22, 44, 666);
 console.log(max);
 
 console.log(Math.round(4.5534));
+
 console.log(Math.floor(2.55));
+
 console.log(Math.ceil(8.542));
+
 console.log(Math.abs(5-10));
+
 console.log(Math.min(3, 5, 1));
+
 console.log(Math.sqrt(16));
+
 console.log(Math.pow(2,3));
---
+++ random.js
@@ -0,0 +1,17 @@
+/**
+ * Math.random() function is used for get random number
+ */
+
+
+const random1= Math.random(); // It will give a random number with fraction btn 0 to 1;
+
+const random2= Math.random()*10; // It will give a random number with fraction btn 1 to 10;
+
+const random3= Math.round(Math.random()*10); //It provide a random number without fraction btn 1 to 10
+
+console.log('Random Number :', random1);
+
+console.log('Random Number :', random2);
+
+console.log('Random Number :', random3);
+
